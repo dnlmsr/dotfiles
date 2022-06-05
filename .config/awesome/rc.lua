2cf9a387a07164fd41a5fbcdeac7919a59c460b7
Daniele Moser <dnlmsr0@gmail.com>
2022-06-05 22:04:15 +0200

    Added autostart applications

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -571,3 +571,8 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Autostart applications
+awful.spawn.once("udiskie -Nt")
+awful.spawn.once("pasystray")
+awful.spawn.once("autorandr -c")
