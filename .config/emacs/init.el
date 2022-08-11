b43329ee9a8ee1dd8aee494756719c6afa4f0c2a
Daniele Moser <dnlmsr0@gmail.com>
2022-08-11 14:13:43 +0200

    Using straight package manager

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,19 +1,21 @@
-;; Package configuration
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
+;; Setup straight.el
+(setq package-enable-at-startup nil)
 
-(setq package-list
-  '(
-    evil
-    ))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; Packages
+(straight-use-package 'evil)
 
 ;; Don't show the splash screen
 (setq inhibit-startup-message t)
