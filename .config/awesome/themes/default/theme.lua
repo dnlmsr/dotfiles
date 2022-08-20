a0b4e82d396a9c7e2f7df0760e150a1015fe8308
Daniele Moser <dnlmsr0@gmail.com>
2022-08-20 10:22:52 +0200

    Awesome: using xresources colors

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -11,24 +11,26 @@ local themes_path = gfs.get_configuration_dir() .. "themes/"
 
 local theme = {}
 
+local xtheme = xresources.get_current_theme()
+
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
+theme.bg_normal     = xtheme["background"]
+theme.bg_focus      = xtheme["color2"]
+theme.bg_urgent     = xtheme["color7"]
+theme.bg_minimize   = xtheme["color8"]
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
-theme.fg_minimize   = "#ffffff"
+theme.fg_normal     = xtheme["foreground"]
+theme.fg_focus      = xtheme["color7"]
+theme.fg_urgent     = xtheme["color0"]
+theme.fg_minimize   = xtheme["color7"]
 
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+theme.border_normal = xtheme["background"]
+theme.border_focus  = xtheme["color2"]
+theme.border_marked = xtheme["color1"]
 
 -- There are other variable sets
 -- overriding the default one when
