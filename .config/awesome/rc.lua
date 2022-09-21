a043e71d924615878fc0e36adff19e4ae58b4b56
Daniele Moser <dnlmsr0@gmail.com>
2022-09-21 15:14:30 +0200

    Awesome: added battery widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -209,6 +209,7 @@ awful.screen.connect_for_each_screen(function(s)
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
+	    require("battery-widget") {},
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
