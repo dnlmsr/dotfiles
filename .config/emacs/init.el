56b571ff10f294c5874dd57a64759e702c9f116c
Daniele Moser <d.moser@ewo.com>
2023-03-13 07:19:35 +0000

    Fixed typo

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -128,8 +128,8 @@
 (use-package flycheck)
 (use-package lsp-mode
   :custom (lsp-pylsp-plugins-pylint-enabled t)
-  :hook (c-mode python-mode)
   (lsp-rust-analyzer-cargo-watch-command "clippy")
+  :hook (c-mode python-mode)
   )
 (use-package lsp-ui)
 
